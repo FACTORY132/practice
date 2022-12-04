f33bba267468c0baf515e712fd8c1751f707bbf8
FACTORY132 <65250110+FACTORY132@users.noreply.github.com>
2022-12-04 13:16:36 +0700

    Update Save.js

--- script/Save.js
+++ script/Save.js
@@ -1,15 +1,8 @@
 var tab = document.getElementById("tab").innerHTML = localStorage['main'] || "Writes";
-var saver = true;
 const log = console.log;
     
 setInterval(function () {
-    if (saver == true) {
         localStorage['main'] = document.getElementById("tab").innerHTML;
-    }
-    else if (saver == false) {
-
-    }
-
 }, 750)
 
 

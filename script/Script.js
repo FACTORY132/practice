6ad5de63a37569ff4d6dbeb9f71e88d43390cc54
FACTORY132 <65250110+FACTORY132@users.noreply.github.com>
2022-12-05 10:52:13 +0700

    Update Script.js

--- script/Script.js
+++ script/Script.js
@@ -1,21 +1,33 @@
 
 function Light_Gray() {
     var light = document.getElementById("light");
+    var dark = document.getElementById("dark");
     if (light.checked == true) {
         document.body.style.background = "red";
+        dark.checked = false;
     }
     else {
         document.body.style.background = "linear-gradient(320deg, rgba(31, 26, 26, 0.95), rgba(83, 82, 82, 1))"
     }
-    console.log(any)
 
 }
 function Dark() {
     var dark = document.getElementById("dark");
+    var light = document.getElementById("light");
+
     if (dark.checked == true) {
         document.body.style.background = "black"
+        light.checked = false
     }
     else {
         document.body.style.background = "linear-gradient(320deg, rgba(31, 26, 26, 0.95), rgba(83, 82, 82, 1))"
     }
 }
+
+
+
+function random(origin, pow) {
+    return Math.floor(Math.random() * Math.pow(origin, pow));
+}
+
+
